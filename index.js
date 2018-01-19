c3e44f6b14c5c5fcee7cf9f37dca547cb35869f7
Merve Erdol <merve-uygun@windowslive.com>
2018-01-19 16:34:40 +0300

    Add mongoose & cookieparser

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const cookieparser = require('cookie-parser');
 
 const app = express();
 app.use(bodyparser.json());
+app.use(cookieparser());
 app.set('view engine', 'pug');
 
 const note = require('./routes/note');
