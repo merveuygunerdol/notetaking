9d61d8b11ccfe133cecf539deb9c39dce34e2ca0
Merve Erdol <merve-uygun@windowslive.com>
2018-01-20 23:01:15 +0300

    Change note-model to mongoose schema

--- models/note-model.js
+++ models/note-model.js
@@ -1,16 +1,7 @@
-module.exports = class Note {
-  constructor(id, title, body) {
-    this.id = id;
-    this.title = title,
-    this.body = body;
-  }
-  printTitle() {
-    console.log(this.title);
-  }
-  printBody() {
-    console.log(this.body);
-  }
-  static create(obj) {
-    return new Note(obj.id, obj.title, obj.body);
-  }
-};
+const mongoose = require('mongoose');
+
+const noteSchema = mongoose.Schema({
+  title: String,
+  body: String,
+});
+module.exports = mongoose.model('Note', noteSchema);
