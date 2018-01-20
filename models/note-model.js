cb1851d1b17389302b096e4f83466362723f1a7e
Merve Erdol <merve-uygun@windowslive.com>
2018-01-20 23:35:24 +0300

    Increment note id automatically

--- models/note-model.js
+++ models/note-model.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const noteSchema = mongoose.Schema({
   title: String,
   body: String,
 });
+noteSchema.plugin(AutoIncrement, { inc_field: 'id' });
 module.exports = mongoose.model('Note', noteSchema);
