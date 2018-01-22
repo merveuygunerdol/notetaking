9f3a162de2d502ef42a78a31b60baaa7dc33280f
Merve Erdol <merve-uygun@windowslive.com>
2018-01-22 12:55:45 +0300

    Connect mongo DB_URL

--- database-connection.js
+++ database-connection.js
@@ -1,3 +1,4 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+const connectionString = process.env.DB_URL || 'mongodb://localhost//notetaking';
 mongoose.connect('mongodb://localhost/notetaking');
