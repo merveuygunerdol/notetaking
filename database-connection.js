b9d3e8000fd91eb9e40b11df01390e5ca7ec8aa6
Merve Erdol <merve-uygun@windowslive.com>
2018-01-22 14:53:58 +0300

    Fix db connection error

--- database-connection.js
+++ database-connection.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const connectionString = process.env.DB_URL || 'mongodb://localhost//notetaking';
-mongoose.connect('mongodb://localhost/notetaking');
+mongoose.connect(connectionString, { useMongoClient: true })
