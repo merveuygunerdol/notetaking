0f11eb7086ff328b1f1ca08bf33779ca04f1a5e0
Merve Erdol <merve-uygun@windowslive.com>
2018-01-22 11:26:46 +0300

    Abstract away app as a module & require it

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const cookieparser = require('cookie-parser');
+
+require('./database-connection');
+const app = express();
+app.use(bodyparser.json());
+app.use(cookieparser());
+app.set('view engine', 'pug');
+
+const note = require('./routes/note');
+app.use('/note', note);
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+module.exports = app;
--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-const express = require('express');
-const bodyparser = require('body-parser');
-const cookieparser = require('cookie-parser');
-
-require('./database-connection');
-const app = express();
-app.use(bodyparser.json());
-app.use(cookieparser());
-app.set('view engine', 'pug');
-
-const note = require('./routes/note');
-app.use('/note', note);
-
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
-
+const app = require('./app');
 app.listen(3030, () => {
   console.log('Server Listening');
 });
