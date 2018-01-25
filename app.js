094967bd7831520a519542624ac3b7871f8230fd
Merve Erdol <merve-uygun@windowslive.com>
2018-01-25 20:30:49 +0300

    Set up registration page

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const cookieparser = require('cookie-parser');
+const cors = require('cors');
 
 require('./database-connection');
 const app = express();
 app.use(bodyparser.json());
 app.use(cookieparser());
 app.set('view engine', 'pug');
+app.use(cors());
 
 const note = require('./routes/note');
 app.use('/note', note);
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.post('/register', (req, res) => {
+  const email = req.body.email;
+  res.send({
+    message: `Hello ${email}`,
+  });
 });
 module.exports = app;
