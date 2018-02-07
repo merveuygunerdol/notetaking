11af41512c2e11d4830f25dbb715991f9a67f701
Merve Erdol <merve-uygun@windowslive.com>
2018-02-07 13:19:14 +0300

    Display notes in the FE

---
+++ vuenote/src/components/api.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+  <ul v-if="posts && posts.length">
+    <li v-for="post of posts" :key="post">
+      <p><strong>{{post.title}}</strong></p>
+      <p>{{post.body}}</p>
+    </li>
+  </ul>
+
+  <ul v-if="errors && errors.length">
+    <li v-for="error of errors" :key="error">
+      {{error.message}}
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+// eslint-disable-next-line
+/* eslint-disable */
+import axios from 'axios';
+import HTTP from '../main.js';
+window.axios = axios
+export default {
+// eslint-disable-next-line
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  created() {
+    axios.get(`http://localhost:3030/note`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+
+    // async / await version (created() becomes async created())
+    //
+    // try {
+    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    //   this.posts = response.data
+    // } catch (e) {
+    //   this.errors.push(e)
+    // }
+  }
+}
+</script>
