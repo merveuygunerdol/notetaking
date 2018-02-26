0b00b50f426991f2ec97ccb7f4697eb05beceb27
Merve Erdol <merve-uygun@windowslive.com>
2018-02-26 12:23:47 +0300

    Fix eslint

--- vuenote/src/main.js
+++ vuenote/src/main.js
@@ -1,9 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import axios from 'axios';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-import axios from 'axios';
 import note from '@/components/note';
 import delButton from '@/components/removeButton';
 
@@ -15,14 +15,14 @@ Vue.config.productionTip = false;
 /* eslint-disable no-new */
 // eslint-disable-next-line
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>',
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>',
 });
 export const HTTP = axios.create({
-  baseURL: 'http://localhost:3030/note',
-  headers: {
-    Authorization: 'Bearer {token}',
-  },
+    baseURL: 'http://localhost:3030/note',
+    headers: {
+        Authorization: 'Bearer {token}',
+    },
 });
