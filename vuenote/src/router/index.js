9a21c576af761d880779e740bf59632926e45a9d
Merve Erdol <merve-uygun@windowslive.com>
2018-03-05 12:21:06 +0300

    Add editNote and noteForm routes

--- vuenote/src/router/index.js
+++ vuenote/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import mainPage from '@/components/mainPage';
 import HelloWorld from '@/components/HelloWorld.vue';
+import noteForm from '@/components/noteForm.vue';
+import editNote from '@/components/editNote.vue'
 
 Vue.use(Router);
 
@@ -16,6 +18,16 @@ export default new Router({
             path: '/note',
             name: 'mainPage',
             component: mainPage,
+        },
+        {
+            path: '/noteForm',
+            name: 'noteForm',
+            component: noteForm,
+        },
+        {
+            path: '/editNote',
+            name: 'editNote',
+            component: editNote,
         },
     ],
 });
