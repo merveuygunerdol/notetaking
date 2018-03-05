7db478032366d676f7f5c5d09e4a8f900fa145b7
Merve Erdol <merve-uygun@windowslive.com>
2018-03-05 12:19:49 +0300

    Add noteForm component

---
+++ vuenote/src/components/noteForm.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+<body>
+               <!-- h2 Add / Edit Note-->
+    <div class="container">
+        <div class="content">
+            <div class="colummn">
+                <div class="column is-half is-offset-one-quarter">
+                    <div class="field">
+  <label class="label">Title</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="Note Title">
+  </div>
+</div>
+ <div class="field">
+  <label class="label">Body</label>
+  <div class="control">
+    <textarea class="textarea" placeholder="Note Body"></textarea>
+  </div>
+</div>
+<div class="control">
+  <!-- button Add / Save--><a class="button">Button</a>
+  </div>
+</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</template>
+<script>
+export default {
+  props: ['button'],
+  data(){
+      return {
+
+      }
+  }
+}
+</script>
