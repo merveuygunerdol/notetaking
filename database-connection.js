923a5ff546fe8a798f59ae0aa8b208ac77a63d86
Merve Erdol <merve-uygun@windowslive.com>
2018-04-26 11:28:40 +0300

    Fix database connection warning

--- database-connection.js
+++ database-connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 mongoose.Promise = global.Promise;
 const connectionString = process.env.DB_URL || 'mongodb://localhost/notetaking';
-mongoose.connect(connectionString, { useMongoClient: true });
+mongoose.connect(connectionString);
